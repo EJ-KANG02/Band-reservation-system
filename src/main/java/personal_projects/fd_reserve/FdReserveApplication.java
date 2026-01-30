a390d05e994c9226366e97e77901e25c55ed482b
eunji <enji4@naver.com>
2026-01-30 14:58:37 +0900

    :tada: Init : 프로젝트 시작

---
+++ src/main/java/personal_projects/fd_reserve/FdReserveApplication.java
@@ -0,0 +1,13 @@
+package personal_projects.fd_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdReserveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal_projects/fd_reserve/FdReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package personal_projects.fd_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdReserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
