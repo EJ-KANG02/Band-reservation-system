343c609d2fb7b7c077d5dc34b777aeabe71592e8
eunji <enji4@naver.com>
2026-02-01 21:29:57 +0900

    :sparkles: Feat : Security Config 기능 구현

---
+++ src/main/java/personal_projects/fd_reserve/global/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package personal_projects.fd_reserve.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import personal_projects.fd_reserve.global.jwt.JwtFilter;
+import personal_projects.fd_reserve.global.jwt.TokenProvider;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v0/auth/signup", "/api/v0/auth/login").permitAll() // 가입/로그인은 누구나 가능
+                        .requestMatchers("/api/v0/admin/**").hasRole("OFFICER") // 어드민은 회장단만 가능
+                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                )
+
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
